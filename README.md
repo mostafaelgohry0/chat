0901541a45975508c3926d974b1964e19bcad076
mostafa elgohry <elgohryeng@gmail.com>
2017-10-15 01:58:34 +0200

    add files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-mycaht
+chat app build by using node.js , express 
---
+++ modules/userdb.js
@@ -0,0 +1,10 @@
+
+var mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient
+var mongodbUrl = 'mongodb://localhost:27017/db1';
+
+///// definding sechema here 
+
+
+
+
